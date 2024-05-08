10ebd6a5b64e59191ca0c89f563d0c741d0bf519
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-05-09 02:42:24 +0900

    🔥 仮のメニューバーと画面遷移を削除

--- wails-project/frontend/src/App.tsx
+++ wails-project/frontend/src/App.tsx
@@ -7,34 +7,20 @@ import {
   TranslateFormat,
 } from "../wailsjs/go/main/App";
 import "./App.css";
-import { MenuBar } from "./component/MenuBar";
 import { TranslateScreen } from "./component/TranslateScreen";
 import { ApiSettingsScreen } from "./component/ApiSettingsScreen";
 
-export type ScreenType = "Translate" | "ApiSettings";
 function App() {
-  const [currentScreen, setCurrentScreen] = useState("Translate" as ScreenType);
   return (
     <Grid
-      templateRows="100px 1fr"
+      gridTemplateRows="3fr 1fr"
       width="calc(100vw - 40px)"
       height="calc(100vh - 40px)"
       margin="20px"
     >
       <GridItem rowSpan={1} colSpan={1}>
-        <MenuBar setCurrentScreen={setCurrentScreen} />
-      </GridItem>
-      <GridItem rowSpan={1} colSpan={1}>
-        {(() => {
-          switch (currentScreen) {
-            case "Translate":
-              return <TranslateScreen />;
-            case "ApiSettings":
-              return <ApiSettingsScreen />;
-            default:
-              return <TranslateScreen />;
-          }
-        })()}
+        <TranslateScreen />;
+        <ApiSettingsScreen />
       </GridItem>
     </Grid>
   );

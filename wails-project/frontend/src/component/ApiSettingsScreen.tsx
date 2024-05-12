68a74b24a11959446b24b6182c8b89abc732ede3
koto623 <akan.02toko@gmail.com>
2024-05-13 02:33:11 +0900

    💄 フォームラベルを追加

--- wails-project/frontend/src/component/ApiSettingsScreen.tsx
+++ wails-project/frontend/src/component/ApiSettingsScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Input } from "@chakra-ui/react";
+import { Button, FormControl, FormLabel, Grid, Input } from "@chakra-ui/react";
 import { useState } from "react";
 import { WriteApiKey } from "../../wailsjs/go/main/App";
 import { textraapi } from "../../wailsjs/go/models";
@@ -16,10 +16,22 @@ export const ApiSettingsScreen = () => {
     WriteApiKey(data);
   };
   return (
-    <Grid>
-      <Input onChange={(e) => setApiKey(e.target.value)} value={apiKey} />
-      <Input onChange={(e) => setApiSecret(e.target.value)} value={apiSecret} />
-      <Input onChange={(e) => setUserName(e.target.value)} value={userName} />
+    <Grid margin={"10vh auto"} width={"50%"} rowGap={"10px"}>
+      <FormControl>
+        <FormLabel>API key</FormLabel>
+        <Input onChange={(e) => setApiKey(e.target.value)} value={apiKey} />
+      </FormControl>
+      <FormControl>
+        <FormLabel>API secret</FormLabel>
+        <Input
+          onChange={(e) => setApiSecret(e.target.value)}
+          value={apiSecret}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>User name</FormLabel>
+        <Input onChange={(e) => setUserName(e.target.value)} value={userName} />
+      </FormControl>
       <Button onClick={sendApiData}>Save</Button>
     </Grid>
   );
